b0f4df159e32fdbc6e394cbe36beb22f3ac2b9fe
David <Davidteri91@gmail.com>
2017-07-01 23:36:44 +0200

    vendor: add Google property overides
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- products/gms.mk
+++ products/gms.mk
@@ -15,6 +15,17 @@
 # vendor path
 VENDOR_SONYAOSP_PATH := vendor/google
 
+# Google property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true
+
 # SELinux
 BOARD_USE_ENFORCING_SELINUX := true
 
