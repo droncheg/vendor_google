168fe6e0108bb7efbd09c2c497972caff855cfa8
David <Davidteri91@gmail.com>
2017-09-05 17:16:03 +0200

    vendor: update bootanimation
    Use Pixel white
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- products/gms.mk
+++ products/gms.mk
@@ -70,13 +70,13 @@ endif
 # bootanimation (240-320 DPI)
 ifneq ($(filter aosp_sgp521 aosp_sgp511_windy aosp_sgp621 aosp_sgp611_windy aosp_d5803 aosp_e23% aosp_e5823 aosp_d5503 aosp_c6833 aosp_sgp771 aosp_sgp712_windy aosp_f5321, $(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES +=  \
-    vendor/google/prebuilt/common/bootanimation/240_320/bootanimation.zip:system/media/bootanimation.zip
+    $(VENDOR_SONYAOSP_PATH)/prebuilt/common/bootanimation/720/bootanimation.zip:system/media/bootanimation.zip
 endif
 
 # bootanimation (480 DPI)
 ifneq ($(filter aosp_c6903 aosp_d6503 aosp_d6603 aosp_e65% aosp_e66% aosp_e68% aosp_f512% aosp_f813% aosp_f833% aosp_g823% aosp_g814%, $(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES +=  \
-    vendor/google/prebuilt/common/bootanimation/480/bootanimation.zip:system/media/bootanimation.zip
+    $(VENDOR_SONYAOSP_PATH)/prebuilt/common/bootanimation/1080/bootanimation.zip:system/media/bootanimation.zip
 endif
 
 # RIL
