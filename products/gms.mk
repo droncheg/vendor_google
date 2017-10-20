5514cc148c1d582cc2d647e40dc6fe01b3bc96fb
David <Davidteri91@gmail.com>
2017-10-20 22:17:37 +0200

    vendor: switch ro.control_privapp_permissions as enforce
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- products/gms.mk
+++ products/gms.mk
@@ -17,7 +17,7 @@ VENDOR_SONYAOSP_PATH := vendor/google
 
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=log \
+    ro.control_privapp_permissions=enforce \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
